576ecf184b554e425dc345072b063d6af4a6e597
Chris Cooper <cjcooper252@gmail.com>
2017-01-23 11:01:05 +0000

    transaction log creates a new transaction

--- lib/transaction_log.rb
+++ lib/transaction_log.rb
@@ -2,8 +2,17 @@ class TransactionLog
 
   attr_reader :transactions
 
-  def initialize
+  def initialize(transaction_class: Transaction)
     @transactions = []
+    @transaction_class = transaction_class
   end
+
+  def add_transaction(amount)
+    transaction_class.new(amount: 10)
+  end
+
+  private
+
+  attr_reader :transaction_class
 
 end
--- spec/transaction_log_spec.rb
+++ spec/transaction_log_spec.rb
@@ -2,10 +2,21 @@ require 'transaction_log'
 
 describe TransactionLog do
 
-  subject(:transaction_log) {described_class.new}
+  let(:transaction) {double :transaction}
+  let(:transaction_class) {double :transaction_class, new: transaction}
+  subject(:transaction_log) {described_class.new(transaction_class: transaction_class)}
 
   it "is empty to begin with" do
     expect(subject.transactions).to be_empty
+  end
+
+  describe '#add_transaction' do
+
+    it "creates a new transaction" do
+      expect(transaction_class).to receive(:new).with(amount: 10);
+      transaction_log.add_transaction(10)
+    end
+
   end
 
 end
