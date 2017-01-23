af393a5804fd1789be644b87de21c496dfaa4b21
Chris Cooper <cjcooper252@gmail.com>
2017-01-23 14:21:41 +0000

    prints out table

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,9 +1,21 @@
 class Statement
 
+  LAYOUT = "%-15s %s %-10s %s %-10s %s %-10s\n"
+
   attr_reader :transaction_log
 
   def initialize(transaction_log)
     @transaction_log = transaction_log
+  end
+
+  def print_statement
+
+    printf(LAYOUT, "date", "||", "credit",  "||", "debit",  "||", "balance")
+
+    transaction_log.transactions.each do |t|
+      printf(LAYOUT, t.date, "||",t.credit,"||", t.debit, "||",t.resulting_balance)
+    end
+
   end
 
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,20 +2,24 @@ require 'statement'
 
 describe Statement do
 
-  let(:transaction_1) {double :transaction}
-  let(:transaction_2) {double :transaction}
+  let(:date) {Time.now.to_date}
+  let(:transaction_1) {double :transaction, date: date, credit: 10, debit: nil, resulting_balance: 100}
+  let(:transaction_2) {double :transaction, date: date, credit: nil, debit: 5, resulting_balance: 95}
   let(:transactions) do [transaction_1, transaction_2] end
   let(:transaction_log) {double :transaction_log, transactions: transactions}
   subject(:statement) {Statement.new(transaction_log)}
 
 
   it "is initialized with a transaction log" do
-    require 'pry'; binding.pry
     expect(subject.transaction_log).to equal(transaction_log)
   end
 
   describe "#print" do
 
+    it "should print a line of the statement" do
+      # expect(STDOUT).to receive(:printf).with("%-10s %s\n", 10, nil, 100)
+      subject.print_statement
+    end
   end
 
 end
